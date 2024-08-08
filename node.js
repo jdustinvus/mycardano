283103895a8595a0758c814705ab8b1e53fc88bc
jdustinvus <jdustin.vus86@gmail.com>
2024-08-07 19:33:25 -0700

    Add files via upload

---
+++ node.js
@@ -0,0 +1,30 @@
+{
+    "network_tip": {
+        "time": "2021-04-12T21:59:25Z",
+        "epoch_number": 125,
+        "absolute_slot_number": 23895549,
+        "slot_number": 265149
+    },
+    "node_era": "mary",
+    "node_tip": {
+        "height": {
+            "quantity": 0,
+            "unit": "block"
+        },
+        "time": "2019-07-24T20:20:16Z",
+        "epoch_number": 0,
+        "absolute_slot_number": 0,
+        "slot_number": 0
+    },
+    "sync_progress": {
+        "status": "syncing",
+        "progress": {
+            "quantity": 0,
+            "unit": "percent"
+        }
+    },
+    "next_epoch": {
+        "epoch_start_time": "2021-04-14T20:20:16Z",
+        "epoch_number": 126
+    }
+}
